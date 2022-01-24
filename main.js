dd857fda4c081cfed717fa17eadce5623c87d92c
Camryn2009 <89714877+Camryn2009@users.noreply.github.com>
2022-01-23 22:09:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("my first console project");
+
+var fname = "camryn";
+var lname = "Ager";
+var fullname = fname.concat(lname);
+console.log(fullname);
+
+var a = 10; var b = 5; var add = a + b; console.log(add); var sub = a - b; console.log(sub); var multiple = a * b; console.log(multiple); var divide = a / b; console.log(divide);
